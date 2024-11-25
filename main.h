660f70c3820c141b2eafa44f46b2caff80f22ff1
Shaan Somarchand <153312506+ShaanSom@users.noreply.github.com>
2024-11-25 14:51:11 +0100

    Clean the mian.h and the main.c library

--- main.h
+++ main.h
@@ -1,16 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+/* Library: */
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-#include <unistd.h>
-/**
- *
- */
 
 
+/* Prototype: */
 int _putchar(char c);
 int _printf(const char *format, ...);
-
 
 #endif
