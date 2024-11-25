deea525fa99588292dc24877d7114119dfb5532c
Alex-git-pro <alexandre.mousse.git@gmail.com>
2024-11-25 21:03:04 +0700

    created printf start

---
+++ _printf.c
@@ -0,0 +1,9 @@
+/**
+ * _printf - function that produces output according to a format
+ * @format: list of types of arguments passed to the function
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
