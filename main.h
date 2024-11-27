95c3af72887b1cbd0ead3012f0a8735846e251f5
Shaan Somarchand <153312506+ShaanSom@users.noreply.github.com>
2024-11-28 00:15:33 +0100

    Added the specifier structure to the header file and added the NULL constant, because the <unistd> library can't be used

--- main.h
+++ main.h
@@ -7,6 +7,22 @@
 #include <limits.h>
 #include <stdarg.h>
 
+/* Specifier Structure */
+typedef struct specifier
+{
+	char specifier;
+	int (*f)(va_list);
+} specifier_t;
+
+
+/**
+ * <unistd> library can't be used:
+ * "Squiggles are disabled for this translation unit"
+ * NULL as been defined manually to be used.
+*/
+#define NULL ((void *)0);
+
+
 /* Prototype: */
 int _putchar(char c);
 int _printf(const char *format, ...);
