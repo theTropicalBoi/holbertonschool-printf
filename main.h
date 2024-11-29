744ceedf6e70b785dc8a4a856ac4483c1e42d5ba
Shaan Somarchand <153312506+ShaanSom@users.noreply.github.com>
2024-11-29 09:28:42 +0100

    Removed the structure and and the unused prototype

--- main.h
+++ main.h
@@ -9,14 +9,6 @@
 #include <limits.h>
 
 
-/* Specifier Structure */
-typedef struct specifier
-{
-	char specifier; /* the format specifier (%s, %d, ...)*/
-	int (*f)(va_list);
-} specifier_t;
-
-
 /* Prototype: */
 int _putchar(char c);
 int _printf(const char *format, ...);
@@ -26,6 +18,5 @@ int format_char(va_list arg_list); /* print the char format */
 int format_string(va_list arg_list); /* print the string format */
 int format_percent(va_list arg_list); /* print the percent */
 int format_int(va_list arg_list); /* print the int format */
-int conv_int_to_str(); /* convert int to string */
 
 #endif
